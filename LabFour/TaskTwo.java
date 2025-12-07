ea5e460eb05acadce1dba0486af961e83ba5cdf2
Adnan <adnanajmain9@gmail.com>
2025-12-08 02:29:07 +0600

    Add IDCard class with display functionality

---
+++ LabFour/TaskTwo.java
@@ -0,0 +1,23 @@
+class IDCard {
+String name;
+int id;
+String department;
+String institution;
+IDCard(String name, int id, String department, String institution) {
+this.name = name;
+this.id = id;
+this.department = department;
+this.institution = institution;
+}
+void showID() {
+System.out.println("Name: " + name);
+System.out.println("ID: " + id);
+System.out.println("Department: " + department);
+System.out.println("Institution: " + institution);
+}
+public static void main(String[] args) {
+IDCard myID = new IDCard("Adnan Ajmain", 251003, "CCE", "International 
+Islamic University Chittagong");
+myID.showID();
+}
+}
