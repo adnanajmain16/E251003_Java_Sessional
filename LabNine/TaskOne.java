5d628eed5d90ff9c970de688f53c8b77915affff
Adnan <adnanajmain9@gmail.com>
2025-12-08 03:06:33 +0600

    Create TaskOne.java

---
+++ LabNine/TaskOne.java
@@ -0,0 +1,35 @@
+class Student {
+ private String name;
+ private String id;
+ private double cgpa;
+ public void setName(String n) {
+ name = n;
+ }
+ public void setId(String i) {
+ id = i;
+ }
+ public void setCgpa(double c) {
+ cgpa = c;
+ }
+ public String getName() {
+ return name;
+ }
+ public String getId() {
+ return id;
+ }
+ public double getCgpa() {
+ return cgpa;
+ }
+}
+public class TestStudent {
+ public static void main(String[] args) {
+ Student s1 = new Student();
+ s1.setName("Adnan");
+ s1.setId("E251003");
+ s1.setCgpa(4.00);
+ System.out.println("Student Info:");
+ System.out.println("Name: " + s1.getName());
+ System.out.println("ID: " + s1.getId());
+ System.out.println("CGPA: " + s1.getCgpa());
+ }
+}
