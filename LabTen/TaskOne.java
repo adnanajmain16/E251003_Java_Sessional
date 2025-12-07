656362c9ebf00c752b4fe0f2583097b357dcbbcc
Adnan <adnanajmain9@gmail.com>
2025-12-08 02:37:20 +0600

    Implement Shape hierarchy with Circle and Rectangle

---
+++ LabTen/TaskOne.java
@@ -0,0 +1,32 @@
+abstract class Shape {
+ abstract void area();
+}
+class Circle extends Shape {
+ double radius;
+ Circle(double radius) {
+ this.radius = radius;
+ }
+ @Override
+ void area() {
+ System.out.println("Area of Circle: " + (3.1416 * radius * radius));
+ }
+}
+class Rectangle extends Shape {
+ double length, width;
+ Rectangle(double length, double width) {
+ this.length = length;
+ this.width = width;
+ }
+ @Override
+ void area() {
+ System.out.println("Area of Rectangle: " + (length * width));
+ }
+}
+public class ShapeTask1 {
+ public static void main(String[] args) {
+ Circle c = new Circle(5);
+ Rectangle r = new Rectangle(4, 6);
+ c.area();
+ r.area();
+ }
+}
